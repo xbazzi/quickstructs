269b22bb28f75e3d601f25f4d84310396b42bc2f
xbazzi <codemaster@xbazzi.com>
2025-12-14 08:22:45 -0700

    chore: minor refactoring changes

--- include/quick/io/Config.hh
+++ include/quick/io/Config.hh
@@ -9,29 +9,26 @@
 #include "fiah/utils/Logger.hh"
 #include "fiah/utils/TomlParser.hh"
 
-namespace fiah::io
-{
+namespace quick::io {
 
-class Config
-{
-private:
-    std::filesystem::path m_config_path{};
-    static inline fiah::utils::Logger<Config>& m_logger{
-        fiah::utils::Logger<Config>::get_instance("Config")
-    };
+class Config {
+ private:
+  std::filesystem::path m_config_path{};
+  static inline fiah::utils::Logger<Config>& m_logger{
+      fiah::utils::Logger<Config>::get_instance("Config")};
 
-    fiah::utils::TomlParser m_parser{};
+  fiah::utils::TomlParser m_parser{};
 
-public:
-    Config() noexcept = default;
-    Config(const std::filesystem::path&);
-    ~Config() = default;
+ public:
+  Config() noexcept = default;
+  Config(const std::filesystem::path&);
+  ~Config() = default;
 
-    bool parse_config() noexcept;
+  bool parse_config() noexcept;
 
-    // Server getters
-    std::string get_market_ip() const noexcept;
-    uint16_t get_market_port() const noexcept;
-    // const std::string& get_market_protocol() const noexcept;
+  // Server getters
+  std::string get_market_ip() const noexcept;
+  uint16_t get_market_port() const noexcept;
+  // const std::string& get_market_protocol() const noexcept;
 };
-} // End io namespace
+}  // namespace quick::io
